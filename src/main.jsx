f057ceb3f074e81ae9e45a136929f3e3e23b4dd0
LightOracle67 <adriancl2001@gmail.com>
2026-07-01 10:49:49 +0200

    Añade comentarios en español para desarrolladores

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Punto de entrada de la SPA: monta la invitacion en el nodo raiz.
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // StrictMode ayuda a detectar efectos secundarios durante el desarrollo.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
