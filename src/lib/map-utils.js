6b97510a7cdd97357bff712d0474412523b6d15d
LightOracle67 <adriancl2001@gmail.com>
2026-07-07 02:53:23 +0200

    fix: mapa previsualizacion desactivado (servidor caido)

--- src/lib/map-utils.js
+++ src/lib/map-utils.js
@@ -1,6 +1,1 @@
-import "leaflet/dist/leaflet.css";
-
-export const buildOpenFreeMapPreviewUrl = async (location) => {
-  if (!location) return "";
-  return `https://staticmap.openstreetmap.de/staticmap.php?center=${location.latitude},${location.longitude}&zoom=14&size=1200x800&maptype=mapnik&markers=${location.latitude},${location.longitude},red-pushpin`;
-};
+export const buildOpenFreeMapPreviewUrl = async () => "";
