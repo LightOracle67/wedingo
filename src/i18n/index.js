0d1fb99bc221752286b13889d01040f3d86e086c
LightOracle67 <adriancl2001@gmail.com>
2026-07-08 03:32:22 +0200

    feat: anyadidos 8 idiomas (fr, pt, de, it, ca, gl, eu, va)

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,12 +3,31 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import es from "./locales/es.json";
 import en from "./locales/en.json";
+import fr from "./locales/fr.json";
+import pt from "./locales/pt.json";
+import de from "./locales/de.json";
+import it from "./locales/it.json";
+import ca from "./locales/ca.json";
+import gl from "./locales/gl.json";
+import eu from "./locales/eu.json";
+import va from "./locales/va.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: { es: { translation: es }, en: { translation: en } },
+    resources: {
+      es: { translation: es },
+      en: { translation: en },
+      fr: { translation: fr },
+      pt: { translation: pt },
+      de: { translation: de },
+      it: { translation: it },
+      ca: { translation: ca },
+      gl: { translation: gl },
+      eu: { translation: eu },
+      va: { translation: va },
+    },
     fallbackLng: "es",
     interpolation: { escapeValue: false },
   });
