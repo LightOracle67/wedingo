3b42f3e3e642af717198a5dbc440f9a619a6e721
LightOracle67 <adriancl2001@gmail.com>
2026-07-08 16:06:25 +0200

    feat: 76 idiomas i18n con grupos reorganizados

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -42,6 +42,41 @@ import ukRaw from "./locales/uk.json";
 import viRaw from "./locales/vi.json";
 import bnRaw from "./locales/bn.json";
 import swRaw from "./locales/sw.json";
+import bgRaw from "./locales/bg.json";
+import etRaw from "./locales/et.json";
+import gaRaw from "./locales/ga.json";
+import cyRaw from "./locales/cy.json";
+import sqRaw from "./locales/sq.json";
+import mkRaw from "./locales/mk.json";
+import bsRaw from "./locales/bs.json";
+import srRaw from "./locales/sr.json";
+import idRaw from "./locales/id.json";
+import tlRaw from "./locales/tl.json";
+import urRaw from "./locales/ur.json";
+import faRaw from "./locales/fa.json";
+import taRaw from "./locales/ta.json";
+import teRaw from "./locales/te.json";
+import mrRaw from "./locales/mr.json";
+import guRaw from "./locales/gu.json";
+import kmRaw from "./locales/km.json";
+import loRaw from "./locales/lo.json";
+import myRaw from "./locales/my.json";
+import kaRaw from "./locales/ka.json";
+import hyRaw from "./locales/hy.json";
+import azRaw from "./locales/az.json";
+import kkRaw from "./locales/kk.json";
+import uzRaw from "./locales/uz.json";
+import amRaw from "./locales/am.json";
+import haRaw from "./locales/ha.json";
+import yoRaw from "./locales/yo.json";
+import igRaw from "./locales/ig.json";
+import zuRaw from "./locales/zu.json";
+import rwRaw from "./locales/rw.json";
+import omRaw from "./locales/om.json";
+import quRaw from "./locales/qu.json";
+import gnRaw from "./locales/gn.json";
+import htRaw from "./locales/ht.json";
+import eoRaw from "./locales/eo.json";
 
 function wrap(data) {
   return { translation: data };
@@ -62,7 +97,15 @@ i18n
       zh: wrap(zhRaw), he: wrap(heRaw), hi: wrap(hiRaw), ja: wrap(jaRaw),
       ko: wrap(koRaw), ms: wrap(msRaw), ru: wrap(ruRaw), th: wrap(thRaw),
       tr: wrap(trRaw), uk: wrap(ukRaw), vi: wrap(viRaw), bn: wrap(bnRaw),
-      sw: wrap(swRaw),
+      sw: wrap(swRaw), bg: wrap(bgRaw), et: wrap(etRaw), ga: wrap(gaRaw),
+      cy: wrap(cyRaw), sq: wrap(sqRaw), mk: wrap(mkRaw), bs: wrap(bsRaw),
+      sr: wrap(srRaw), id: wrap(idRaw), tl: wrap(tlRaw), ur: wrap(urRaw),
+      fa: wrap(faRaw), ta: wrap(taRaw), te: wrap(teRaw), mr: wrap(mrRaw),
+      gu: wrap(guRaw), km: wrap(kmRaw), lo: wrap(loRaw), my: wrap(myRaw),
+      ka: wrap(kaRaw), hy: wrap(hyRaw), az: wrap(azRaw), kk: wrap(kkRaw),
+      uz: wrap(uzRaw), am: wrap(amRaw), ha: wrap(haRaw), yo: wrap(yoRaw),
+      ig: wrap(igRaw), zu: wrap(zuRaw), rw: wrap(rwRaw), om: wrap(omRaw),
+      qu: wrap(quRaw), gn: wrap(gnRaw), ht: wrap(htRaw), eo: wrap(eoRaw),
     },
     fallbackLng: "es",
     interpolation: { escapeValue: false },
