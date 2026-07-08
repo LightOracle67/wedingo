79af3e74aac1b1dd1feb042268dde0e206955200
LightOracle67 <adriancl2001@gmail.com>
2026-07-08 14:57:06 +0200

    fix: eliminado script inline que violaba CSP

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,10 @@ function buildTimestamp() {
   return {
     name: "build-timestamp",
     transformIndexHtml(html) {
-      return html
-        .replace(
-          "</head>",
-          `<meta name="deploy-id" content="${ts}" />\n  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />\n  <meta http-equiv="Pragma" content="no-cache" />\n  <meta http-equiv="Expires" content="0" />\n  </head>`
-        )
-        .replace(
-          "</body>",
-          `<script>var v="${ts}";if(localStorage.getItem("wedin_deploy")!==v){localStorage.setItem("wedin_deploy",v);location.reload(true)}</script>\n</body>`
-        );
+      return html.replace(
+        "</head>",
+        `<meta name="deploy-id" content="${ts}" />\n  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />\n  <meta http-equiv="Pragma" content="no-cache" />\n  <meta http-equiv="Expires" content="0" />\n  </head>`
+      );
     },
   };
 }
