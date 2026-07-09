8cee04f40787ac08e741155c9764e33ff4fe9471
LightOracle67 <adriancl2001@gmail.com>
2026-07-09 15:37:51 +0200

    chore: bump version a 2.1.0

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 import { WEDDING_MESSAGES } from "./invite-messages";
 
-export const APP_VERSION = "2.0.1";
+export const APP_VERSION = "2.1.0";
 
 export const ALLOWED_UPLOAD_TYPES = new Set(["image/jpeg", "image/png", "image/webp"]);
 export const MAX_UPLOAD_SIZE_BYTES = 20 * 1024 * 1024;
