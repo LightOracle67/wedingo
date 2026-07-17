9b2f594936b6a297719c95e5cbbe32c055b125ac
LightOracle67 <adriancl2001@gmail.com>
2026-07-17 10:39:49 +0200

    v2.4.3: bump for cache invalidation

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.4.2";
+export const APP_VERSION = "2.4.3";
 
 export const ALLOWED_UPLOAD_TYPES = new Set(["image/jpeg", "image/png", "image/webp"]);
 export const MAX_UPLOAD_SIZE_BYTES = 20 * 1024 * 1024;
