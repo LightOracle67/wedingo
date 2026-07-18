d250d2efff9406003d13517379f266d384e574ea
LightOracle67 <adriancl2001@gmail.com>
2026-07-19 01:17:00 +0200

    fireflies: 4 trayectorias distintas con brillo variable, 24 unidades

--- src/components/Fireflies.jsx
+++ src/components/Fireflies.jsx
@@ -13,8 +13,8 @@ const Fireflies = memo(function Fireflies() {
           style={{
             left: `${(i * 7.3 + 3.1) % 100}%`,
             top: `${(i * 11.7 + 8.9) % 100}%`,
-            animationDelay: `${-(i * 2.7) % 35}s`,
-            animationDuration: `${28 + (i % 7) * 2}s`,
+            animationDelay: `${-(i * 2.3 + 0.7) % 35}s`,
+            animationDuration: `${30 + (i % 6) * 4}s`,
           }}
         />
       ))}
