2a2dfad3047ad0e8d255c973cc7d42125d8cbed1
LightOracle67 <adriancl2001@gmail.com>
2026-08-17 16:42:55 +0200

    chore: bump v2.99.7

--- src/lib/changelog.ts
+++ src/lib/changelog.ts
@@ -1,5 +1,13 @@
 export const CHANGELOG = [
   {
+    version: "2.99.7",
+    date: "2026-08-17",
+    changes: [
+      "TODO: describe los cambios de esta versión",
+    ],
+  },
+
+  {
     version: "2.99.6",
     date: "2026-08-16",
     changes: [
